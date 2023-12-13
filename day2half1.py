e23cb416ffe0e4f08bd04457a5465093edd6be0b
aaron <146187041+dartmonkeyExeter@users.noreply.github.com>
2023-12-13 10:05:48 +0000

    Update day2half1.py

--- day2half1.py
+++ day2half1.py
@@ -1,17 +1,48 @@
+from copy import deepcopy
+
 file = open("input.txt")
 
 # 12 max red, 13 max green, 14 max blue
 
-for line in file:
-    line = line.strip()
-    split = line.split(": ")[1]
-    split = split.split(", ")
-    for i in range(len(split)):
-        if ";" in split[i]:
-            try:
-                split[i] = split[i].split("; ")[0]
-                split = split.insert(i + 1, split[i].split("; ")[1])
-            except IndexError:
-                continue
+colour_to_max = {
+    "red": 12,
+    "green": 13,
+    "blue": 14,
+}
+answer = 0
 
-    print(split)
+for line in file:
+
+    line = line.strip()
+
+    ID = line.strip("Game ")
+    ID = int(ID.split(": ")[0])
+
+    games = line.split(": ")[1]
+    games = games.split(", ")
+
+    new_games = []
+
+    for i in games:
+        if ";" in i:
+            formatted = i.split("; ")
+            new_games.append(formatted[0])
+            new_games.append(formatted[1])
+        else:
+            new_games.append(i)
+
+    impossible = False
+
+    for idx, i in enumerate(new_games):
+        amount = int(i.split(" ")[0])
+        colour = i.split(" ")[1]
+        maximum = colour_to_max[colour]
+
+        if amount > maximum:
+            impossible = True
+            break
+    if impossible is False:
+
+        answer += ID
+
+print(answer)
